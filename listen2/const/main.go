f20898bf43e9dbb782af0490a87d6a0ffd2035fb
Jie.Z <346025232@qq.com>
2020-11-04 22:14:36 +0800

    first commit

---
+++ listen2/const/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+				const a int = 100
+				const b string = "hello"
+
+			const (
+				a int    = 100
+				b string = "hello"
+			)
+
+		const (
+			a int = 100
+			b
+		)
+	*/
+	const (
+		a int = 100
+		b
+		c int = 200
+		d
+	)
+	fmt.Printf("a=%d b=%d c=%d d=%d\n", a, b, c, d)
+
+	const (
+		e = iota
+		f
+		g
+	)
+
+	const (
+		a1 = 1 << iota
+		a2
+		a3
+	)
+	fmt.Printf("e=%d f=%d g=%d\n", e, f, g)
+	fmt.Printf("a1=%d a2=%d a3=%d\n", a1, a2, a3)
+
+}
